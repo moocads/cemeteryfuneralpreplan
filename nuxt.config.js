bb3208227ddd87703aa408c62ce9886febf85807
moocads <info@moocads.com>
2020-09-18 11:16:09 -0400

    update

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,11 +5,11 @@ export default {
   ** Headers of the page
   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'Pre-Plan Cemetery & Funeral | 预置墓园及殡葬服务',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content:  '预置墓园及殡葬服务，土葬服务，骨灰安葬服务' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
