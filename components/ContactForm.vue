9a51b588f1dea1119ee10572f57cfb1b7508f808
moocads <info@moocads.com>
2020-09-22 10:59:19 -0400

    change form

--- components/ContactForm.vue
+++ components/ContactForm.vue
@@ -55,7 +55,7 @@
     <div class="note">{{$t('note')}}</div>
     <b-form v-if="show" class="js-cm-form" id="subForm"
       action="https://www.createsend.com/t/subscribeerror?description=" method="post"
-      data-id="2BE4EF332AA2E32596E38B640E9056191FFF111727AF487101544A982DEFFB1AF9BB3F79C30689341D3CCB06DDF830E4393AD6AA09BD823E96A2A5A2B643F098">
+      data-id="2BE4EF332AA2E32596E38B640E905619A20F87F2FB0945001E0ED88929D24AC24B98AE7ACB02CB69E75149B4E8EE58DA6A52BAF45A918F4669BFCF6DA5BA1F8D">
       <b-row>
         <b-col>
 
@@ -63,7 +63,7 @@
               <template v-slot:label>
     {{$t('firstn')}}<span class="text-danger">*</span>
   </template>
-            <b-form-input id="fieldjyulihu" maxlength="200" name="cm-f-jyulihu" required placeholder="Enter first name">
+            <b-form-input id="fieldjyuiuyj" maxlength="200" name="cm-f-jyuiuyj" required placeholder="Enter first name">
             </b-form-input>
           </b-form-group>
         </b-col>
@@ -72,22 +72,22 @@
                           <template v-slot:label>
     {{$t('lastn')}}<span class="text-danger">*</span>
   </template>
-            <b-form-input  id="fieldjyuliku" maxlength="200" name="cm-f-jyuliku" required placeholder="Enter last name"></b-form-input>
+            <b-form-input  id="fieldjyuiuyt" maxlength="200" name="cm-f-jyuiuyt" required placeholder="Enter last name"></b-form-input>
           </b-form-group>
         </b-col>
       </b-row>
       <b-form-group id="input-group-1" description="We'll never share your email with anyone else.">
-                                  <template v-slot:label>
+    <template v-slot:label>
     {{$t('email')}}<span class="text-danger">*</span>
   </template>
-        <b-form-input autocomplete="Email" aria-label="Email" class="js-cm-email-input qa-input-email" id="fieldEmail" maxlength="200" name="cm-ykyhkki-ykyhkki" type="email" required placeholder="Enter email"></b-form-input>
+        <b-form-input autocomplete="Email" aria-label="Email" class="js-cm-email-input qa-input-email" id="fieldEmail" maxlength="200" name="cm-ykjtkrr-ykjtkrr" type="email" required placeholder="Enter email"></b-form-input>
       </b-form-group>
 
       <b-form-group id="input-group-2">
                                           <template v-slot:label>
     {{$t('phone')}}<span class="text-danger">*</span>
   </template>
-        <b-form-input id="fieldjyuliul" maxlength="200" name="cm-f-jyuliul" required placeholder="Phone number"></b-form-input>
+        <b-form-input id="fieldjyuiuyi" maxlength="200" name="cm-f-jyuiuyi" required placeholder="Phone number"></b-form-input>
       </b-form-group>
 
 
@@ -97,15 +97,15 @@
   </template>
         <b-form-select id="fieldjyuldrl" name="cm-fo-jyuldrl" value="" required>
           <option value="">{{$t('select')}}</option>
-          <option value="2938613">{{$t('ser01')}}</option>
-          <option value="2938614">{{$t('ser02')}}</option>
-          <option value="2938615">{{$t('ser03')}}</option>
-          <option value="2938616">{{$t('ser04')}}</option>
-          <option value="2938617">{{$t('ser05')}}</option>
-          <option value="2939685">{{$t('ser06')}}</option>
+          <option value="2942072">{{$t('ser01')}}</option>
+          <option value="2942073">{{$t('ser02')}}</option>
+          <option value="2942074">{{$t('ser03')}}</option>
+          <option value="2942075">{{$t('ser04')}}</option>
+          <option value="2942076">{{$t('ser05')}}</option>
+          <option value="2942077">{{$t('ser06')}}</option>
         </b-form-select>
       </b-form-group>
-      <b-form-textarea id="fieldjyuldrr" maxlength="200" name="cm-f-jyuldrr" placeholder="Send us your message ..." rows="3" max-rows="6">
+      <b-form-textarea id="fieldjyuiuyd" maxlength="200" name="cm-f-jyuiuyd" placeholder="Send us your message ..." rows="3" max-rows="6">
       </b-form-textarea>
       <br>
       <b-row>
