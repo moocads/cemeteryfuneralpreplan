b8ef128792335605c3a9781e41f09279e76868f3
Gary Zhang <gary@moocads.com>
2021-05-25 13:27:40 -0400

    add google analytics

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,93 +1,98 @@
-
 export default {
-  mode: 'universal',
+  mode: "universal",
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
-    title: 'Pre-Plan Cemetery & Funeral | 预置墓园及殡葬服务',
+    title: "Pre-Plan Cemetery & Funeral | 预置墓园及殡葬服务",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content:  '预置墓园及殡葬服务，土葬服务，骨灰安葬服务' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content: "预置墓园及殡葬服务，土葬服务，骨灰安葬服务"
+      }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ],
-    script:[
-      {src:'https://js.createsend1.com/javascript/copypastesubscribeformlogic.js', type:'text/javascript'}
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+    script: [
+      {
+        src: "https://www.googletagmanager.com/gtag/js?id=UA-197873382-1",
+        async: "true"
+      },
+      {
+        src:
+          "https://js.createsend1.com/javascript/copypastesubscribeformlogic.js",
+        type: "text/javascript"
+      }
     ]
   },
   /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#fff' },
+   ** Customize the progress-bar color
+   */
+  loading: { color: "#fff" },
   /*
-  ** Global CSS
-  */
-  css: [
-    '@/assets/css/main.scss'
-  ],
+   ** Global CSS
+   */
+  css: ["@/assets/css/main.scss"],
   /*
-  ** Plugins to load before mounting the App
-  */
+   ** Plugins to load before mounting the App
+   */
   plugins: [
-    {src:'~plugins/smoothscroll.js', ssr:false},
+    { src: "~plugins/smoothscroll.js", ssr: false },
     { src: "~plugins/aos", ssr: false },
     { src: "~plugins/antcomponent.js", ssr: false },
-    { src: '~plugins/ga.js', mode: 'client' }
+    { src: "~plugins/ga.js", mode: "client" }
   ],
   /*
-  ** Nuxt.js dev-modules
-  */
-  buildModules: [
-  ],
+   ** Nuxt.js dev-modules
+   */
+  buildModules: [],
   /*
-  ** Nuxt.js modules
-  */
+   ** Nuxt.js modules
+   */
   modules: [
     // Doc: https://bootstrap-vue.js.org
-    'bootstrap-vue/nuxt',
-    '@nuxtjs/axios',
+    "bootstrap-vue/nuxt",
+    "@nuxtjs/axios",
     [
-      'nuxt-i18n',
+      "nuxt-i18n",
       {
         vueI18nLoader: true,
         locales: [
           {
-            code: 'zh',
-            iso: 'zh-CN',
-            name: 'ZH'
+            code: "zh",
+            iso: "zh-CN",
+            name: "ZH"
           },
           {
-            code: 'tc',
-            iso: 'zh-Hant',
-            name: 'TC'
+            code: "tc",
+            iso: "zh-Hant",
+            name: "TC"
           }
         ],
-        defaultLocale: 'zh',
+        defaultLocale: "zh",
         detectBrowserLanguage: {
           useCookie: false,
-          cookieKey: 'i18n_redirected',
+          cookieKey: "i18n_redirected",
           alwaysRedirect: false,
-          fallbackLocale: 'zh'
+          fallbackLocale: "zh"
         }
       }
-    ],
+    ]
   ],
   /*
-  ** Build configuration
-  */
+   ** Build configuration
+   */
   build: {
     /*
-    ** You can extend webpack config here
-    */
-    extend (config, ctx) {
-    }
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {}
   },
   server: {
     port: process.env.PORT || 3000,
-    host: process.env.HOST || '0.0.0.0',
+    host: process.env.HOST || "0.0.0.0",
     timing: false
   }
-}
+};
